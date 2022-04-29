5a823c3e6ef40c340f2d168ce7935ef0ae1f5998
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-04-29 09:06:37 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -438,5 +438,22 @@ class OffsetMenu extends Option
 	}
 }
 
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
 
+	public override function press():Bool
+	{
+                trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
 
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
+}
