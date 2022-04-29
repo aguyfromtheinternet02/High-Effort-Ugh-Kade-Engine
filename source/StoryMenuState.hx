ce55eb416f427db543ba3acbaa86ef2aa7f3ddaa
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-04-29 09:02:38 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -178,6 +178,10 @@ class StoryMenuState extends MusicBeatState
 
 		trace("Line 165");
 
+                #if android
+	        addVirtualPad(FULL, A_B);
+                #end
+
 		super.create();
 	}
 
